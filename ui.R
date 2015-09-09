ca65781bbae4fadf06b8d217dfa3588760942bf7
Melody <Melody@1x-vl946-128-119-250-56.wireless.umass.edu>
2015-09-09 18:34:40 -0400

    test

--- ui.R
+++ ui.R
@@ -10,7 +10,7 @@ library(shiny)
 shinyUI(fluidPage(
 
   # Application title
-  titlePanel("Old Geyser"),
+  titlePanel("Test"),
 
   # Sidebar with a slider input for number of bins
   sidebarLayout(
