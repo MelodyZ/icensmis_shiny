11e8fe8c6da225a67e3d132ef94e6cba3d730039
Melody <Melody@Melodys-MacBook-Pro.local>
2015-09-16 21:49:57 -0400

    sketch

--- server.R
+++ server.R
@@ -8,16 +8,38 @@
 library(shiny)
 
 shinyServer(function(input, output) {
-
-  output$distPlot <- renderPlot({
-
-    # generate bins based on input$bins from ui.R
-    x    <- faithful[, 2]
-    bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-    # draw the histogram with the specified number of bins
-    hist(x, breaks = bins, col = 'darkgray', border = 'white')
+  
+  output$values <- renderText({
+    
+    # Calculate Power and Sample Size
+    surv <- exp(log(0.9) * (1:8)/8)
+    ssize <- icpower(HR = as.numeric(input$HR), 
+                     sensitivity = input$sen, 
+                     specificity = input$spe, 
+                     survivals = surv, 
+                     power = input$power)
+    result_ss <- ssize$result[[1]]
+    
+    paste("Your sample size is:", as.character(result_ss), ".")
 
   })
-
+  
+  Nlist <- seq(100, 15000, by = 100)
+  
+  output$disPlot <- renderPlot({
+    
+    lambda.base <- -log(input$neven)/max(input$tim)
+    survivals <- 1 - pexp(input$tim, lambda.base)
+    plot(0, 0, type = "n", xlab = "N", ylab = "Power", xlim = c(0, max(Nlist)), 
+         cex.lab = 1.4, cex.main = 1.5, ylim = c(0, 1))
+    
+    pow <- icpower(HR = 1.25, 
+                   sensitivity = input$sen, 
+                   specificity = input$spe, 
+                   survivals, 
+                   Nlist)$result
+    lines(pow$N, pow$power, lwd = 2)
+    legend("topleft", cex = 1.25, legend = paste("(", input$sen, ", ", input$spe, ")"), lwd = 1.5)
+  })
+ 
 })
--- ui.R
+++ ui.R
@@ -10,21 +10,31 @@ library(shiny)
 shinyUI(fluidPage(
 
   # Application title
-  titlePanel("Test"),
+  titlePanel("Sample Size Estimation"),
 
   # Sidebar with a slider input for number of bins
   sidebarLayout(
     sidebarPanel(
-      sliderInput("bins",
-                  "Number of bins:",
-                  min = 1,
-                  max = 50,
-                  value = 30)
-    ),
+      textInput("HR",
+                  "Hazard ratio:",
+                  value = 1.25),
+      sliderInput("sen", "Sensitivity:",
+                  min = 0, max = 1, value = 0.55),
+      sliderInput("spe", "Specificity:",
+                  min = 0, max = 1, value = 0.99),
+      sliderInput("power", "Power:",
+                  min = 0, max = 1, value = 0.9),
+
+      sliderInput("tim", "Times:",
+                  min = 1, max = 20, value = c(1,8)),
+      sliderInput("neven", "Noevent:",
+                  min = 0, max = 1, value = 0.9)
+      ),
 
     # Show a plot of the generated distribution
     mainPanel(
-      plotOutput("distPlot")
+      textOutput("values"),
+      plotOutput("disPlot")
     )
   )
 ))
