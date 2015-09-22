db4aee3c09ab8544b35e8c1c0d51d78ad63d780a
Melody <Melody@uma-vl942-128-119-72-24.wireless.umass.edu>
2015-09-22 16:55:31 -0400

    figure 3

--- server.R
+++ server.R
@@ -64,7 +64,7 @@ shinyServer(function(input, output) {
     noevent <- seq(input$nevent[1], input$nevent[2], by = 0.05)
     ssize <- sapply(noevent, function(x) icpower(HR, input$sen3, input$spe3, exp(log(x) * testtimes/max(testtimes)), power = 0.9)$result$N)
     plot(1 - noevent, ssize, type = "n", xlab = expression(paste("Cumulative incidence (", 1 - S[J + 1], ")")), ylab = "Sample size", main = "(b)", ylim = c(0, 14000), cex.lab = 1.25)
-    lines(1 - noevent, ssize)
+    lines(1 - noevent, ssize, lwd = 2)
     legend("topright", legend = paste("(", input$sen3, ", ", input$spe3, ")"), lwd = 2)
   })
    ## Sample size as function of number of tests
@@ -79,8 +79,81 @@ shinyServer(function(input, output) {
       icpower(HR, input$sen3, input$spe3, survivals, power = 0.9)$result$N
     })
     plot(ntest, ssize, type = "n", ylim = c(8000, 20000), xlab = "Number of tests", ylab = "Sample size", main = "(c)", cex.lab = 1.25)
-    lines(ntest, ssize)
+    lines(ntest, ssize, lwd = 2)
     legend("topright", legend = paste("(", input$sen3, ", ", input$spe3, ")"), lwd = 2)
   })
+  
+  # 3.3 Relative efficiency of test schedule 1 and schedule 2
+  icpower.schedule <- function(schedules, ratios, HR, sensitivity, specificity, survivals, alpha = 0.05, power = 0.9) {
+    ntest <- length(survivals)
+    If <- matrix(0, nrow = ntest + 1, ncol = ntest + 1)
+    for (i in seq_along(schedules)) {
+      id <- sort(schedules[[i]])
+      mid <- c(1, id + 1)
+      pow <- icpower(HR = HR, sensitivity = sensitivity, specificity = specificity,
+                     survivals = survivals[id], power = 0.9)
+      If[mid, mid] <- If[mid, mid] + ratios[i] * (pow$I1 + pow$I2)/2
+    }
+    inv.If <- solve(If)
+    beta.var <- inv.If[1]
+    N <- ceiling((qnorm(1 - alpha/2) + qnorm(power))^2 * beta.var/log(HR)^2) 
+    N
+  }
+   ## Relative sample size as function of hazard ratio
+  output$disPlot3_1 <- renderPlot({
+  noevent <- 0.9
+  testtimes <- 1:8
+  survivals <- exp(log(noevent) * testtimes/max(testtimes)) 
+  HR <- seq(input$HRange[1], input$HRange[2], by = 0.05)
+  ssize.HR <- function(HR, sensitivity, specificity) {
+    schedules <- list(c(1, 3, 5, 7), c(2, 4, 6, 8))
+    ratios <- c(0.5, 0.5)
+    ssize1 <- icpower.schedule(schedules, ratios, HR, sensitivity, specificity, survivals)
+    ssize2 <- icpower(HR, sensitivity, specificity, survivals[schedules[[2]]], power = 0.9)$result$N 
+    ssize1/ssize2
+  }
+  ratio <- sapply(HR, function(x) ssize.HR(x, input$sen4, input$spe4))
+  plot(HR, ratio, type = "n", ylim = c(1.06, 1.20), xlab = "HR", ylab = "Sample size ratio", main = "(a)") 
+  lines(HR, ratio, lwd = 2)
+  legend("topright", legend = paste("(", input$sen4, ", ", input$spe4, ")"), lwd = 2)
+  })
+   ## Relative sample size as function of cumulative incidence
+  output$disPlot3_2 <- renderPlot({
+    HR <- 1.25
+    testtimes <- 1:8
+    noevent <- seq(input$nevent[1], input$nevent[2], by = 0.05)
+    ssize.cumi <- function(noevent, sensitivity, specificity) { 
+      survivals <- exp(log(noevent) * testtimes/max(testtimes)) 
+      schedules <- list(c(1, 3, 5, 7), c(2, 4, 6, 8))
+      ratios <- c(0.5, 0.5)
+      ssize1 <- icpower.schedule(schedules, ratios, HR, sensitivity, specificity, survivals)
+      ssize2 <- icpower(HR, sensitivity, specificity, survivals[schedules[[2]]], power = 0.9)$result$N 
+      ssize1/ssize2
+    }
+    ratio <- sapply(noevent, function(x) ssize.cumi(x, input$sen4, input$spe4))
+    plot(1 - noevent, ratio, type = "n", xlab = expression(paste("Cumulative incidence (", 1 - S[J + 1], ")")), ylab = "Sample size ratio", main = "(b)", cex.lab = 1.25, ylim = c(1, 1.33))
+    lines(1 - noevent, ratio, lwd = 2)
+    legend("topright", legend = paste("(", input$sen4, ", ", input$spe4, ")"), lwd = 2)
+  })
+   ## Relative sample size as function of number of tests
+  output$disPlot3_3 <- renderPlot({
+    HR <- 1.25
+    noevent <- 0.9
+    ntest <- seq(input$ntest4[1], input$ntest4[2], by = 2)
+    ssize.ntest <- function(ntest, sensitivity, specificity) { 
+      testtimes <- 1:ntest
+      survivals <- exp(log(noevent) * testtimes/max(testtimes)) 
+      schedules <- list(seq(1, ntest, by = 2), seq(2, ntest, by = 2))
+      ratios <- c(0.5, 0.5)
+      ssize1 <- icpower.schedule(schedules, ratios, HR, sensitivity, specificity, survivals)
+      ssize2 <- icpower(HR, sensitivity, specificity, survivals[schedules[[2]]], power = 0.9)$result$N
+      ssize1/ssize2
+    }
+    ratio <- sapply(ntest, function(x) ssize.ntest(x, input$sen4, input$spe4))
+    plot(ntest, ratio, type = "n", ylim = c(1, 1.3), xlab = "Number of test points", ylab = "Sample size ratio", main = "(c)")
+    lines(ntest, ratio, lwd = 2)
+    legend("topright", legend = paste("(", input$sen4, ", ", input$spe4, ")"), lwd = 2)
+  })
+   
  
 })
--- ui.R
+++ ui.R
@@ -22,7 +22,8 @@ shinyUI(fluidPage(
         selectInput("select", "Choose a study you want to conduct",
                     choices = list("Sample Size Estimation" = 1,
                                    "Figure 1. Comparison of power versus total sample size (N) for different values of the (sensitivity, specificity) of the diagnostic test" = 2,
-                                   "Figure 2. Effects of hazard ratio, event rate, and number of tests on sample size for different (sensitivity, specificity)" = 3)),
+                                   "Figure 2. Effects of hazard ratio, event rate, and number of tests on sample size for different (sensitivity, specificity)" = 3,
+                                   "Figure 3. Relative efficiency of test schedule 1 and schedule 2" = 4)),
         
         conditionalPanel(
           condition = "input.select == 1",
@@ -69,13 +70,47 @@ shinyUI(fluidPage(
           conditionalPanel(
             condition = "input.effect == 3",
             sliderInput("ntest", "Notest:",
-                        min = 1, max = 20, step = 1, 
+                        min = 2, max = 20, step = 2, 
                         value = c(2, 10))
           ),
           
           sliderInput("sen3", "Sensitivity:",
                       min = 0, max = 1, value = 0.55),
           sliderInput("spe3", "Specificity:",
+                      min = 0, max = 1, value = 0.99)
+        ),
+        
+        conditionalPanel(
+          condition = "input.select == 4",
+          helpText("* Schedules: a list of test time indices for each schedule."),
+          helpText("* Ratios: ratio of subjects assigned to each schedule."),
+          br(),
+          radioButtons("effect4", "Effect of",
+                       choices = list("Hazard Ratio" = 1,
+                                      "Cumulative Incidence" = 2,
+                                      "Number of Tests" = 3)),
+          conditionalPanel(
+            condition = "input.effect4 == 1",
+            sliderInput("HRange", "Hazard Ratio Range:",
+                        min = 1, max = 3, step = 0.05, 
+                        value = c(1.5, 2.5))
+          ),
+          conditionalPanel(
+            condition = "input.effect4 == 2",
+            sliderInput("nevent", "Noevent:",
+                        min = 0, max = 1, step = 0.05, 
+                        value = c(0.2, 0.9))
+          ),
+          conditionalPanel(
+            condition = "input.effect4 == 3",
+            sliderInput("ntest4", "Notest:",
+                        min = 2, max = 20, step = 2, 
+                        value = c(4, 16))
+          ),
+          
+          sliderInput("sen4", "Sensitivity:",
+                      min = 0, max = 1, value = 0.55),
+          sliderInput("spe4", "Specificity:",
                       min = 0, max = 1, value = 0.99)
         )
       ),
@@ -93,6 +128,7 @@ shinyUI(fluidPage(
                    condition = "input.select == 2",
                    plotOutput("disPlot1")
                  ),
+                 
                  conditionalPanel(
                    condition = "input.select == 3",
                    
@@ -108,9 +144,27 @@ shinyUI(fluidPage(
                      condition = "input.effect == 3",
                      plotOutput("disPlot2_3")
                    )
+                   ),
+                 
+                 conditionalPanel(
+                   condition = "input.select == 4",
+                   
+                   conditionalPanel(
+                     condition = "input.effect4 == 1",
+                     plotOutput("disPlot3_1")
+                   ),
+                   conditionalPanel(
+                     condition = "input.effect4 == 2",
+                     plotOutput("disPlot3_2")
+                   ),
+                   conditionalPanel(
+                     condition = "input.effect4 == 3",
+                     plotOutput("disPlot3_3")
                    )
-                 )),
+                 )
+                 ),
         tabPanel("Tutorial")
+      )
       )
     )
 ))
